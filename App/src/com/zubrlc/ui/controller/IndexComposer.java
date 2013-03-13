53c3e12ac0a2a1647b1d1aa8ddfc10df1b56e54e
zubrlc <zubrlc@gmail.com>
2013-03-13 14:28:00 +0200

    new index. With Composer

--- App/src/com/zubrlc/ui/controller/IndexComposer.java
+++ App/src/com/zubrlc/ui/controller/IndexComposer.java
@@ -1,5 +1,30 @@
 package com.zubrlc.ui.controller;
 
-public class IndexComposer {
-	String s;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.GenericAutowireComposer;
+import org.zkoss.zul.*;
+
+
+public class IndexComposer extends GenericAutowireComposer<Component>{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2338020871869270202L;
+
+	public Label l1;
+	
+	public void doAfterCompose(Component comp) throws Exception{
+		super.doAfterCompose(comp);
+		System.out.println("IndexComposer");
+		init();
+		
+	}
+	
+	public void init(){
+		System.out.println("l1="+l1);
+		l1.setValue("Текст");
+	}
+
+	
 }
